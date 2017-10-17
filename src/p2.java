e4ec9a4a06d8afaac8d5d2ab54786615fbc9a61e
Wayne Yang <wengzhong2001@126.com>
2017-10-17 17:36:10 +0800

    Update p2.java

--- src/p2.java
+++ src/p2.java
@@ -7,11 +7,17 @@ public class p2{
         int a=0, b=0, c=0, t=0;
         t = scin.nextInt();
         do{
-            String str = scin.nextString();
+            String str = scin.next();
             for (int i=0; i<str.length();i++){
-                if(str.charAt(i)=='(') a=a+1;
+                if(str.charAt(i)=='('){
+                    a=a+1;
+                    if(i+1<str.length()&&str.charAt(i+1)==']')a=a+1;
+                }
                 if(str.charAt(i)==')') a=a-1;
-                if(str.charAt(i)=='[') b=b+1;
+                if(str.charAt(i)=='['){
+                    b=b+1;
+                    if(i+1<str.length()&&str.charAt(i+1)==')')b=b+1;
+                }
                 if(str.charAt(i)==']') b=b-1;
             }
             if(a==0&&b==0){
@@ -22,6 +28,6 @@ public class p2{
             c++;
             a=0;
             b=0;
-        }
-    }while (c<t)
+        }while (c<t);
+    }
 }
